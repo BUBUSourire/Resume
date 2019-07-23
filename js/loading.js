8e01945cff173235415c167ba60ba2ae5806d233
Sourire <953810390@qq.com>
2019-07-23 22:22:13 +0800

    1

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,4 @@
 // 加载动画
 setTimeout(function () {
     my$("load").className = ("active");
-}, 2000)
+}, 1000)
---
+++ js/message.js
@@ -0,0 +1,36 @@
+var APP_ID = 'HJU7ymq2AK0eLxt56zApolMP-gzGzoHsz';
+var APP_KEY = 'VnF9ruEhOwj1RQpg7EWjpryf';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+//监听用户提交事件--监听form
+let myForm = document.querySelector('#postMessageForm')
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault()//清除默认样式
+    let content = myForm.querySelector('input[name=content]').value//获取用户输入的内容
+    var Message = AV.Object.extend('Message')//创建表，表名为Message
+    var message = new Message()//在表中创建一行数据
+    message.save({
+        'content': content
+    }).then(function (object) {
+        alert('提交成功')
+        window.location.reload()  //成功后自动刷新页面
+    })
+
+})
+
+//将留言内容显示在页面
+var query = new AV.Query('Message');
+query.find().then(function (messages) {
+    //  console.log(messages)
+    let array = messages.map((item) => item.attributes)//注意map用法，创建键值对，存储客户传进的留言,形成数组
+    array.forEach((item) => {//遍历数组，对每一项进行操作
+        let li = document.createElement('li')
+        li.innerText = item.content
+        let messageList = document.querySelector('#messageList')
+        messageList.append(li)
+    })
+})
