e7fd1223117ebb9aa9a87ea11069a675929cbfb2
Sourire <953810390@qq.com>
2019-08-15 11:06:45 +0800

    change

--- Babel_change/dist/js/base/View.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-/**
- * var view = View('选择器名称')
- */
-
-window.View = function (selector) {
-  return document.querySelector(selector);
-};
--- Babel_change/dist/js/loading.js
+++
@@ -1,5 +0,0 @@
-// // 加载动画
-// setTimeout(function () {
-//     my$("load").className = ("active");
-// }, 0)
-"use strict";
