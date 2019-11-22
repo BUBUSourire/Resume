ee69f43cea4d108167285d0b884a8e25879ed081
Sourire <953810390@qq.com>
2019-11-22 18:05:40 +0800

    nothing

--- js/base/View.js
+++
@@ -1,7 +0,0 @@
-/**
- * var view = View('选择器名称')
- */
-
-window.View = function (selector) {
-    return document.querySelector(selector)
-}
--- js/loading.js
+++
@@ -1,4 +0,0 @@
-// 加载动画
-setTimeout(function () {
-    my$("load").className = ("active");
-}, 3000)
